ffc8d28bcbff1e96ae895bd825908f47df06ccd9
Prashanthan <arspprashanthan@gmail.com>
2026-02-07 19:28:15 +0530

    added missing ParseHTML

--- internal/analyzer/analyzer.go
+++ internal/analyzer/analyzer.go
@@ -3,19 +3,23 @@ package analyzer
 import "context"
 
 // Analyze performs the complete analysis of a given URL.
-// It will orchestrate fetching the HTML, parsing the content,
+// it will orchestrate fetching the HTML, parsing the content,
 // checking links, and aggregating the results.
 //
-// The context parameter allows cancellation and timeout control.
+// context parameter allows cancellation and timeout control.
 func Analyze(ctx context.Context, url string) error {
-	// TODO:
 	// 1. Fetch HTML content
-	_, _, err := FetchHTML(ctx, url)
+	html, _, err := FetchHTML(ctx, url)
 	if err != nil {
 		return err
 	}
 
 	// 2. Parse document structure
+	_, _, _, _, err = ParseHTML(html)
+	if err != nil {
+		return err
+	}
+
 	// 3. Analyze links
 	// 4. Detect login forms
 	// 5. Return aggregated result
